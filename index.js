f0db170dca372f617f3ad4b374d24db97379d110
nithinpp69 <nithinpp69@gmail.com>
2020-05-08 11:58:48 +0530

    cmpleted

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ var express = require('express');
 var app = express();
 var server = app.listen(process.env.PORT || 3000);
 app.use(express.static('public'));
+
+var socket = require('socket.io');
+var io = socket(server);
+io.sockets.on('connection', (socket) => {
+  console.log('socket ', socket.id);
+  socket.on('draw', (data) => {
+    socket.broadcast.emit('draw', data);
+  });
+});
--- public/scripts/board.js
+++ public/scripts/board.js
@@ -1,6 +1,5 @@
-const rows = 12;
-const cols = 12;
 var board;
+var socket;
 
 function centerCanvas() {
   var x = (windowWidth - width) / 2;
@@ -10,19 +9,27 @@ function centerCanvas() {
 
 function setup() {
   cnv = createCanvas(windowWidth, windowHeight);
+  socket = io.connect('https://nithinpp69.github.io/blackboard');
+  socket.on('draw', (data) => {
+    noStroke();
+    fill(0, 0, 255);
+    ellipse(data.x, data.y, 5, 5);
+  });
+  background(0);
   centerCanvas();
-  setupBoard();
 }
 
 function setupBoard() {
   redraw();
 }
 
-function draw() {
-  background(0);
+function wnidowResized() {
+  centerCanvas();
 }
 
 function mouseDragged() {
+  noStroke();
   fill(255);
-  ellipse(mouseX, mouseY, 60, 60);
+  socket.emit('draw', { x: mouseX, y: mouseY });
+  ellipse(mouseX, mouseY, 5, 5);
 }
